515f47d86a355f3ba0e673796c9c96624baea3f5
Kuba <jakubm16@interia.pl>
2019-08-17 02:36:07 +0200

    wersja2

--- script.js
+++ script.js
@@ -1,95 +1,164 @@
-
-
-function kolor(x)
-{  
-    var zle=document.querySelectorAll(".odp.redBackground");
-    var dobre=document.querySelectorAll(".odp.popr.redBackground");
+$(function(){
     
-   
-   
     
-    switch(x)
-        {
-            case 1:
-                
-                
-                 for(var i=2; i<=4; i++)
-                    {
-                        var pierwsze = document.getElementById("pyt"+x+"odp"+i);    //wszystkie zmieniają od 1 do 4 na klasę odp bad
-                        pierwsze.className = "odp bad";
-                        
-                    }
-                
-                
-            break;
-            
-            case 2:
-                
-                 for(var i=1; i<=3; i++)
-                    {
-                        var pierwsze = document.getElementById("pyt2odp"+i);
-                         pierwsze.className = "odp bad";
-                    }
-                
-            break;
-            
-            
-        }
-   
-   
+     $(window).scroll(function(){
+       if($(this).scrollTop()>700){
+           jQuery(".scroll").fadeIn();
+       } else{$(".scroll").fadeOut();}
+    });
     
+    
+  $(".quiz_solution").hide();
+    $("input:radio").on("click", function(){
         
-            if (zle.length == 1)
-         {
-            for (var i=0; i<1; i++)
-                {
-             zle[i].className = "odp bad";
-            dobre[i].className = "odp bad popr" ;
-         } }
-    
-    for(var i=1; i<9; i++)
+        if($(this).prop("checked", true))
         {
-            kk=document.getElementById("")
+            $(this).parent().parent().siblings().children().removeClass("redBackground");
+            $(this).parent().addClass("redBackground");
         }
            
+    });
+    
+    $(".dalej").on("submit", function(e){
+        e.preventDefault();
+        if($(".redBackground").length<10)
+            {
+                alert("Nie zaznaczono wszystkich odpowiedzi! Odpowiedz na pytani");
+                return;
+            }
+        let $zdobyte_punkty = $(".odp.popr.redBackground").length;
+        $("#test").append("Brawo zdobyłeś " + $zdobyte_punkty + " pkt!");
+       
+        $(".odp.popr").addClass("green");
+        $(".odp.redBackground").addClass("wrong");
         
-}
+        $("input:radio").prop("disabled", true);
+        $(".quiz_solution").fadeIn();
+        $('window').animate({scrollTop: 0}, 400);
+    });
     
+    $("img.red").on("click",function(){
+        var audio = document.getElementById("play");
+        audio.volume = 0.1;
+        $(this).toggleClass("zielen");
+       $('#play').get(0).pause();
+        if($(this).hasClass("zielen"))
+         {
+             $(this).attr("src", "img/Główna/indexgreen.png");
+             $("#play").get(0).play();
+         }
+        else{
+                $(this).attr("src", "img/Główna/indexred.png");      
+            }             
+    });
+    $(window).on("scroll",function(){
+       if($(window).scrollTop() > 400 )
+           {$("header>img").fadeIn();}
+    });
     
+   $("a.episode").hide(); 
+    $("button").on("click", function(){
+        let aa = $(this).parent().find("a.episode");
+        aa.each(function(index){
+            $(this).delay(200*index).fadeIn(1000)
+        });
+        });
+        });
+       
+   
+        
 
     
 
-function poznaj_odpowiedz()
-{
-     
-    var ksak = document.getElementsByClassName("odp popr redBackground");
-       
-           document.getElementById("test").textContent = "Brawo zdobyłeś " + ksak.length + " pkt!" 
-  
-       // POPRAWNE USTAWIANIE ZIELONEJ NA PODSTAWIE SELEKTORA 
-    var correct=document.querySelectorAll(".odp.popr"); //wybór
-       
-    if(correct.length > 0)
-    {
-        for(var i=0; i<correct.length; i++)
-        correct[i].className="odp green";
-    } 
-    var correct=document.querySelectorAll(".odp.popr"); //wybór
-       
-    if(correct.length > 0)
-    {
-        for(var i=0; i<correct.length; i++)
-        correct[i].className="odp green";
-    }
+
+
+//  $answer.each(function(){})
+//    // POBRANIE WSZYSTKICH INPUTÓW
+//    let kolejne_inputy=document.getElementsByTagName('input');
+//    let len = kolejne_inputy.length;
+//    
+//    
+//    // WYSZUKANIE POLA INPUT ZAZNACZONEGO (WYBRANEJ PRZEZ UŻYTKOWNIKA ODPOWIEDZI) I NADANIE JEGO RODZICOWI KLASY REDBACKGROUND ODPOWIEDZIALNEJ ZA ŻÓŁTE OBRAMOWANIE, A W PRZYPADKU INPUTA NIEZAZNACZONEGO - USUNIĘCIE KLASY REDBACKGROUND RODZICOWI
+//    for(let i=0; i<len; i++)
+//        {
+//            let zazn=kolejne_inputy[i].checked;
+//            // console.log(zazn);
+//            
+//            //RODZIC
+//            let div=kolejne_inputy[i].parentNode;
+//            //console.log(div);
+//            
+//            
+//            if(zazn)
+//            {
+//               //przejście na rodziców inputów(divy)
+//               div.classList.add('redBackground');
+//            }
+//
+//            else
+//            {
+//               div.classList.remove('redBackground');
+//            }
+//
+//        }
+//}
+
+        
+        
     
-    
-    for (var j=0; j<11; j++)
-        {
-            
-            var nr_pytania = document.getElementsByClassName("odp bad redBackground");  //odp2 odp3 odp4
-            if(nr_pytania)
-            nr_pytania[j].setAttribute("class", "odp wrong");
-       
-        }
- 
-}
+
+//
+//
+//    // WYŚWIETLENIE KOMUNIKATU O ILOŚCI ZDOBYTYCH PUNKTÓW
+//    let zdobyte_punkty = document.getElementsByClassName("odp popr redBackground");
+//    
+//    document.getElementById("test").textContent = "Brawo zdobyłeś " + zdobyte_punkty.length + " pkt!" 
+//  
+//    
+//    
+//  
+//    // USTAWIANIE POPRAWNYCH ODPOWIEDZI NA ZIELONO NA PODSTAWIE SELEKTORA 
+//    let correct=document.querySelectorAll(".odp.popr"); //wybór
+//    let len1 = correct.length;    
+//    
+//    for(let i=0; i<len1; i++)
+//    {
+//       correct[i].className="odp green"; 
+//    }
+//    
+//    
+//    
+//   
+//    // USTAWIANIE ZŁYCH ODPOWIEDZI NA CZERWONO NA PODSTAWIE SELEKTORA
+//    let incorrect = document.querySelectorAll(".odp.redBackground");
+//    let len2 = incorrect.length;
+//    
+//    for (let j=0; j<len2; j++)
+//        {        
+//             
+//            if(incorrect)
+//            {
+//                incorrect[j].classList.add("wrong");  
+//            }
+//            
+//       
+//        }
+//    
+//    
+//    
+//    
+//    // DEAKTYWACJA MOŻLIWOŚCI KLIKNIĘCIA NA INPUTY (WYBORU ODPOWIEDZI) 
+//    let inputy=document.querySelectorAll('input[type=radio]');
+//    let len3 = inputy.length;
+//        
+//    for(let i=0; i<len3; i++)
+//    {
+//        inputy[i].disabled=true;
+//    }
+//
+//}
+//
+//const btn = document.querySelector("button");
+//btn.addEventListener('click', poznaj_odpowiedz, false);
+//
+// 
