c47f37d813aec43322e1a364def5d8430a540457
KKuba <jakubm16@interia.pl>
2019-08-26 23:08:32 +0200

    08.2019

--- script.js
+++ script.js
@@ -1,142 +1,133 @@
-$(function(){
-    
+$(function () {
+
     // ScrollToTop Button
-    $(window).on("scroll",function(){
-         
-       if($(this).scrollTop()>700)
-       {
-           $(".scroll").fadeIn();
-       } 
-         
-       else
-       {
-           $(".scroll").fadeOut();
-       }
-         
+    $(window).on("scroll", function () {
+
+        if ($(this).scrollTop() > 700) {
+            $(".scroll").fadeIn();
+        } else {
+            $(".scroll").fadeOut();
+        }
+
     });
-    
-//    Zamiast w main.css html{scroll-behavior: smooth;}
-//    $(".scroll").on('click',function() {
-//    $([document.documentElement, document.body]).animate({
-//        scrollTop: $("body").offset().top}, 1100);
-//});
-    
-   
+
+    //    Zamiast w main.css html{scroll-behavior: smooth;}
+    //    $(".scroll").on('click',function() {
+    //    $([document.documentElement, document.body]).animate({
+    //        scrollTop: $("body").offset().top}, 1100);
+    //});
+
+
     // Quiz
     $(".quiz_solution").hide();
-    
+
     //kolorowanie odpowiedzi
-    $("input:radio").on("click", function(){
-        
-        if($(this).prop("checked", true))
-        {                                                                                         $(this).parent().parent().siblings().children().removeClass("redBackground");
-            $(this).parent().addClass("redBackground");
+    $("input:radio").on("click", function () {
+
+        if ($(this).prop("checked", true)) {
+            $(this).parent().parent().siblings().children().removeClass("choose");
+            $(this).parent().addClass("choose");
         }
-           
+
     });
-    
+
     //podsumowanie quizu
-    $(".dalej").on("click", function(e){
+    $(".dalej").on("submit click", function (e) {
         e.preventDefault();
-        if($(".redBackground").length<10)
-            {
-                alert("Nie zaznaczono wszystkich odpowiedzi! Odpowiedz na pytani");
-                return;
-            }
-        let $zdobyte_punkty = $(".odp.popr.redBackground").length;
-        $("#test").append("Brawo zdobyłeś " + $zdobyte_punkty + " pkt!");
-       
+//        if ($(".choose").length < 10) {
+//            alert("Nie zaznaczono wszystkich odpowiedzi!");
+//            return;
+//        }
+        let $zdobyte_punkty = $(".odp.popr.choose").length;
+        $("#pkt").append("Brawo zdobyłeś " + $zdobyte_punkty + " pkt!");
+
         $(".odp.popr").addClass("green");
-        $(".odp.redBackground").addClass("wrong");
-        
+        $(".odp.choose").addClass("wrong");
+
         $("input:radio").prop("disabled", true);
-        $(".quiz_solution").fadeIn(); 
-        
+        $(".quiz_solution").fadeIn();
+
         $([document.documentElement, document.body]).animate({
-            scrollTop: $("body").offset().top}, 1100);
-        
+            scrollTop: $("body").offset().top
+        }, 1100);
+
         $(this).hide();
         //odpowiedzi
-        $(".quiz_section").each(function(){
-            
-        
-        let $span = $('<span></span>');
-        $span.insertAfter($(this).find(".question"));
-        
-        
-        let value1 = $(this).find(".popr").children().val();
-        let value2 = $(this).find(".redBackground").children().val();
-        
-            
-        if(value2 === value1)
-            {
-                $(this).find($("span")).text("Dobrze! Poprawna odpowiedź to "+ value1);
-                $(this).find($("span")).addClass("dobrze");
-            }
-        else
-            {
-                $(this).find($("span")).text("Źle! Zaznaczyłeś: "+value2+". Poprawna odpowiedź to: "+ value1);
-                $(this).find($("span")).addClass("zle");
+        $(".quiz_section").each(function () {
+
+
+            let $span = $('<span></span>');
+            $span.insertAfter($(this).find(".question"));
+
+
+            let value1 = $(this).find(".popr").children().val();
+            let value2 = $(this).find(".choose").children().val();
+
+
+            if (value2 === value1) {
+                $(this).find($("span")).text("Dobrze! Poprawna odpowiedź to " + value1);
+                $(this).find($("span")).addClass("correct");
+            } else {
+                $(this).find($("span")).text("Źle! Zaznaczyłeś: " + value2 + ". Poprawna odpowiedź to: " + value1);
+                $(this).find($("span")).addClass("incorrect");
             }
         });
-        
+
         $(".quiz_header p").hide();
     });
-    
-    
+
+
     //index.html laughtrack audio
-    $("img.red").on("click",function(){
-        
+    $("img.red").on("click", function () {
+
         var audio = document.getElementById("play");
         audio.volume = 0.1;
-        $(this).toggleClass("zielen");
+        $(this).toggleClass("verde");
         $('#play').get(0).pause();
-        
-        if($(this).hasClass("zielen"))
-         {
-             $(this).attr("src", "img/Główna/indexgreen.png");
-             $("#play").get(0).play();
-             $(this).attr("alt", "Odtwarzanie w trakcie");
-         }
-        else
-         {
-             $(this).attr("src", "img/Główna/indexred.png"); 
-             $(this).attr("alt", "Odtwarzanie zatrzymane");
-         }             
+
+        if ($(this).hasClass("verde")) {
+            $(this).attr("src", "img/Glowna/indexgreen.png");
+            $("#play").get(0).play();
+            $(this).attr("alt", "Odtwarzanie w trakcie");
+        } else {
+            $(this).attr("src", "img/Glowna/indexred.png");
+            $(this).attr("alt", "Odtwarzanie zatrzymane");
+        }
     });
-    
+
     //index.html pierwszy obraz
-    $(window).on("scroll",function(){
-       if($(window).scrollTop() > 400 )
-           {$("header>img").fadeIn();}
+    $(window).on("scroll", function () {
+        if ($(window).scrollTop() > 400) {
+            $("header>img").fadeIn();
+        }
     });
-    
-    
+
+
     //subiektywne.html linki do odcinków
-    $("a.episode").hide(); 
-    $("button").on("click", function(){
+    $("a.episode").hide();
+    $("button").on("click", function () {
         let aa = $(this).parent().find("a.episode");
-        aa.each(function(index){
-            $(this).stop().delay(200*index).fadeToggle(1000)
+        aa.each(function (index) {
+            $(this).stop().delay(200 * index).fadeToggle(1000)
         });
-        });
-    
+    });
+
     //kontakt.html - placeholder
-    $("input").on("focus", function(){
-        let $place=$(this).attr("placeholder");
+    $("input").on("focus", function () {
+        let $place = $(this).attr("placeholder");
         $(this).attr("placeholder", "");
-       
-        $(this).on("blur", function(){
+
+        $(this).on("blur", function () {
             $(this).attr("placeholder", $place);
         });
-    
+
     });
-    
+
     //kontakt.html - textarea
-    $("textarea").on("keydown", function(){
+    $("textarea").on("keydown keyup click mousemove", function () {
         let tekstWprowadzany = $(this).val();
-        let counter = (250 - (tekstWprowadzany.length));    
-    $("#znaki").text("Pozostało: "+counter+" znaków");
+        let counter = (250 - (tekstWprowadzany.length));
+        $("#znaki").text("Pozostało: " + counter + " znaków");
 
     });
 });
